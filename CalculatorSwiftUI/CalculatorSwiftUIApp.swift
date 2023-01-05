844e7ad3a337d0aa43172d2f42651e36719a9752
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2023-01-05 23:43:44 +0100

    Initial Commit

---
+++ CalculatorSwiftUI/CalculatorSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorSwiftUIApp.swift
+//  CalculatorSwiftUI
+//
+//  Created by Джон Костанов on 5/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
