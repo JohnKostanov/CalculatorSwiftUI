ebcac3a35470d96988f0dce528d060dddb6c8521
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2023-01-09 10:52:48 +0100

    Added view-model CalculatorActions

---
+++ CalculatorSwiftUI/ViewModel/CalculatorActions.swift
@@ -0,0 +1,16 @@
+//
+//  CalculatorActions.swift
+//  CalculatorSwiftUI
+//
+//  Created by Джон Костанов on 9/1/23.
+//
+
+import SwiftUI
+
+struct CalculatorActions: CalculatableActions {
+    var data: Calculator
+    
+    func calculateAction(button: ActionButton) {
+        
+    }
+}
