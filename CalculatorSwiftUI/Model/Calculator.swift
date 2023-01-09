1250886e45abf71f1dc13ba8448fb824cf3a9fda
John Kostanov <47560053+JohnKostanov@users.noreply.github.com>
2023-01-09 10:36:07 +0100

    Added model Calculator

--- CalculatorSwiftUI/Model/Calculator.swift
+++ CalculatorSwiftUI/Model/Calculator.swift
@@ -6,3 +6,21 @@
 //
 
 import Foundation
+
+struct Calculator: Calculatable {
+    var resultString: String
+    var button: ActionButton
+}
+
+
+struct ActionButton {
+    enum Labels: String {
+        case comma = ",", zero = "0", one = "1", two = "2", three = "3", four = "4", five = "5", six = "6", seven = "7", eight = "8", nine = "9"
+        case ac = "AC", plusMinus = "±", percent = "%", division = "÷", multiplication = "×", subtraction = "−", addition = "+", equals = "="
+    }
+    
+    var label: Labels
+    var action: (String) -> Void
+}
+
+
